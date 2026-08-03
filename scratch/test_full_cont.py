4db80a6424a97b2d247c56fdfe6926b7b4fcce3b
InmobiliariaICDE <icdesaladenegocios@gmail.com>
2026-08-03 17:46:58 -0500

    grafico de nuevo

---
+++ scratch/test_full_cont.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+sys.path.append('scratch')
+
+with open('admin.html', 'r', encoding='utf-8', errors='ignore') as f:
+    lines = f.readlines()
+
+cont_lines = lines[15420:17230]
+code = "".join(cont_lines)
+
+from debug_stack import parse_debug
+parse_debug(code, 15421)
