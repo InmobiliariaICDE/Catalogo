860347df82ab3182a8e5587314c67930d14e8338
InmobiliariaICDE <icdesaladenegocios@gmail.com>
2026-08-30 09:20:51 -0500

    urtu

--- scratch/inspect_matrix_rendering.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-with open('admin.html', 'r', encoding='utf-8', errors='ignore') as f:
-    lines = f.readlines()
-
-for idx in range(11250, min(11340, len(lines))):
-    sys.stdout.buffer.write(f"Line {idx+1}: {lines[idx].rstrip()}\n".encode('utf-8'))
